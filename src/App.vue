129a3616b4e1c39a79dfcc1ac82a6f9d3e8b7144
Rodion Bulakh <bulakhrodion1@gmail.com>
2021-01-07 13:40:11 +0300

    :bug: fonts  bug fixed

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,6 @@ export default {
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500&display=swap");
 .app {
   font-family: "Oswald", sans-serif;
   -webkit-font-smoothing: antialiased;
